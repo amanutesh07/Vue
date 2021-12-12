378e3fdea9e7a74bf4a9352ed4d89e8c254d5b15
amanutesh07 <91691303+amanutesh07@users.noreply.github.com>
2021-12-12 16:39:44 +0600

    Add files via upload

---
+++ AppAbout.vue
@@ -0,0 +1,38 @@
+<template>
+  <div>
+  <section id="about">
+    <div class="row">
+      <div class="three columns">
+      </div>
+      <div class="nine columns main-col">
+        <h2>About Me</h2>
+        <p>I am a punctual and motivated individual who is able to work in a busy environment and produce high standards of work. I am an excellent team worker and am able to take instructions from all levels and build up good working relationships with all colleagues. I am flexible, reliable and possess excellent time keeping skills.
+        </p>
+        <div class="row">
+          <div class="columns contact-details">
+            <h2>Contact Details</h2>
+            <p class="address">
+              <span>Utesh Aman</span><br>
+              <span>Maktaly, Atameken 20<br>
+						         Zhetisay
+                     </span><br>
+              <span>+7 705 267 02 00</span><br>
+              <span>amanutesh072@gmail.com</span>
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppAbout"
+}
+</script>
+
+<style scoped>
+
+</style>
