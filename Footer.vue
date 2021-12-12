0ff452a60ff68e95d6bd057e7c3433c8f9480744
amanutesh07 <91691303+amanutesh07@users.noreply.github.com>
2021-12-12 16:40:38 +0600

    Add files via upload

---
+++ Footer.vue
@@ -0,0 +1,25 @@
+<template>
+  <footer>
+    <div class="row">
+      <div class="twelve columns">
+        <ul class="social-links">
+          <li><a href="#"><i class="fa fa-facebook"></i></a></li>
+          <li><a href="#"><i class="fa fa-github"></i></a></li>
+          <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+          <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+          <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+        </ul>
+      </div>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: "Footer"
+}
+</script>
+
+<style scoped>
+
+</style>
