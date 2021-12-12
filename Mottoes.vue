772daa6ffa911917e3e01f5fa1a1735346699985
amanutesh07 <91691303+amanutesh07@users.noreply.github.com>
2021-12-12 16:41:08 +0600

    Add files via upload

---
+++ Mottoes.vue
@@ -0,0 +1,53 @@
+<template>
+  <section id="testimonials">
+
+    <div class="text-container">
+
+      <div class="row">
+
+        <div class="two columns header-col">
+
+          <h1><span>Client Testimonials</span></h1>
+
+        </div>
+
+        <div class="ten columns flex-container">
+
+          <div class="flexslider">
+
+            <ul class="slides">
+
+              <li>
+                <blockquote>
+                  <p>Don't confuse schooling with education. I didn't go to Harvard but the people that work work for me did. Failure is an option here. If things are not failing, you are not innovating enough.
+                  </p>
+                  <cite>Elon Musk</cite>
+                </blockquote>
+              </li> <!-- slide ends -->
+
+              <li>
+                <blockquote>
+                  <p>Succes is most ofteh achieved by those who don't know that failure is inevitable. You age doesn't define your maturity, your grades don't define your intelligence, and rumors don't define who you are.
+                  </p>
+                  <cite>Coco Chanel</cite>
+                </blockquote>
+              </li>
+
+            </ul>
+
+          </div>
+        </div>
+      </div>
+    </div>
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Mottoes"
+}
+</script>
+
+<style scoped>
+
+</style>
