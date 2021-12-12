35d7b59c47301c248a4d9881026315386cb08091
amanutesh07 <91691303+amanutesh07@users.noreply.github.com>
2021-12-12 16:39:07 +0600

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div id="app">
+    <AppHeader/>
+    <AppAbout/>
+    <Resume/>
+    <Mottoes/>
+    <Contact/>
+    <Footer/>
+  </div>
+</template>
+
+<script>
+import Vue from 'vue';
+import AppHeader from "./components/AppHeader.vue";
+import AppAbout from "./components/AppAbout.vue";
+import Resume from "./components/Resume.vue";
+import Mottoes from "./components/Mottoes.vue";
+import Contact from "./components/Contact.vue";
+import Footer from "./components/Footer.vue";
+
+export  default   {
+  components:{Footer, Mottoes, AppHeader, AppAbout,Resume, Contact}
+}
+</script>
