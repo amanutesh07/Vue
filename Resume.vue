a2487fc9d4be47030349280bc83bcc1a7281ae7d
amanutesh07 <91691303+amanutesh07@users.noreply.github.com>
2021-12-12 16:41:25 +0600

    Add files via upload

---
+++ Resume.vue
@@ -0,0 +1,175 @@
+<template>
+  <section id="resume">
+    <div class="row education">
+
+      <div class="three columns header-col">
+        <h1><span>Education</span></h1>
+      </div>
+
+      <div class="nine columns main-col">
+
+        <div class="row item">
+
+          <div class="twelve columns">
+
+            <h3>Suleyman Demirel University</h3>
+            <p class="info">Faculty of Engineering and Natural Sciences <span>&bull;</span> <em class="date">Sep 2020 - Jun 2024</em></p>
+
+            <p>
+            <ul>
+              <li>- Major: Information technologies and systems</li>
+              <br>
+              <li>- Mathematics: Calculus, Linear Algebra, Number Theory, Mathematical Logic</li>
+              <br>
+              <li>- Computer Science: Algorithms and Data Structures, Programming in Java, Python Practice</li>
+            </ul>
+            </p>
+
+          </div>
+
+        </div>
+
+        <div class="row item">
+
+          <div class="twelve columns">
+
+            <h3>IT-START</h3>
+            <p class="info">Front-end developer<span>&bull;</span> <em class="date">Jun 2019 - Aug 2019</em></p>
+            <p>
+            <ul>- Studied how to create websites in HTML, CSS, JS and PHP</ul>
+            <ul>- For the database of the individual project used MySQL</ul>
+            <ul>- Made 3+ simple projects and maintained, optimized, troubleshoot and improved websites</ul>
+            </p>
+
+          </div>
+
+        </div>
+
+      </div>
+
+    </div>
+
+
+    <div class="row work">
+
+      <div class="three columns header-col">
+        <h1><span>Projects</span></h1>
+      </div>
+
+      <div class="nine columns main-col">
+
+        <div class="row item">
+
+          <div class="twelve columns">
+
+            <h3>Spring Boot WebSocket Chat Appplication</h3>
+            <p class="info">SpringBoot, Hibernate, HTML, CSS, JS <span>&bull;</span> <em class="date">Sep 2021 - Nov 2021</em></p>
+
+            <p>
+            <ul>
+              <li>- Developed the Controller for sending and receiving messages</li>
+              <li>- Created ChatMessage models</li>
+              <li>- Added listeners for WebSocket events</li>
+            </ul>
+            </p>
+
+          </div>
+
+        </div> <!-- item end -->
+
+        <div class="row item">
+
+          <div class="twelve columns">
+
+            <h3>2D Tank game</h3>
+            <p class="info">Java Core, OOP, JavaFx <span>&bull;</span> <em class="date">Jun 2021 - Aug 2021</em></p>
+
+            <p>
+            <ul>
+              <li>- Used JavaFX to create an application</li>
+              <li>- Added ”Multiplayer” mod and created Socket server with multithreading</li>
+              <li>- Provided all the concepts of Object Oriented Programming</li>
+            </ul>
+            </p>
+
+          </div>
+
+        </div> <!-- item end -->
+
+        <div class="row item">
+
+          <div class="twelve columns">
+
+            <h3>BaykonurX</h3>
+            <p class="info">HTML5, CSS3, JS, Bootstrap5 <span>&bull;</span> <em class="date">Oct 2021</em></p>
+
+            <p>
+            <ul>
+              <li>- Layout 6 page site with support for mobile adaptation</li>
+              <li>- Used Bootstrap for modal window</li>
+            </ul>
+            </p>
+
+          </div>
+
+        </div> <!-- item end -->
+
+      </div> <!-- main-col end -->
+
+    </div> <!-- End Work -->
+    <div class="row skill">
+
+      <div class="three columns header-col">
+        <h1><span>ACHIEVEMENTS</span></h1>
+      </div>
+
+      <div class="nine columns main-col">
+
+        <p>
+        <ul>
+          <li><p style="font-weight: bolder; display: inline;">- Java Certficate from Hackerrank</p></li>
+          <li><p style="font-weight: bolder; display: inline;">- 2nd place in the Republican Math Olympiad ”Akbota” (more than 400 students participated)</p></li>
+          <li><p style="font-weight: bolder; display: inline;">- Python Certficate from Hackerrank</p></li>
+          <li><p style="font-weight: bolder; display: inline;">- Certificate for participation on Facebook Hackercup 2021 </p></li>
+          <li><p style="font-weight: bolder; display: inline;">- 170 solved problems in leetcode.com </p></li>
+        </ul>
+        </p>
+      </div>
+      <br><br>
+    </div>
+
+
+    <div class="row skill">
+
+      <div class="three columns header-col">
+        <h1><span>Skills</span></h1>
+      </div>
+
+      <div class="nine columns main-col">
+
+        <p>
+        <ul>
+          <li><p style="font-weight: bolder; display: inline;">Languages: </p> Java, Python, JavaScript, MySQL, PHP</li>
+          <li><p style="font-weight: bolder; display: inline;">Knowledge: </p> Algorithms, Data Structures, Object Oriented programmin</li>
+          <li><p style="font-weight: bolder; display: inline;">Frameworks: </p> Spring, SpringBoot, JavaFX, Hibernate</li>
+          <li><p style="font-weight: bolder; display: inline;">Front-end: </p> HTML5, CSS3, Bootstrap5</li>
+        </ul>
+        </p>
+
+      </div>
+
+    </div>
+
+
+  </section>
+</template>
+
+<script>
+export default {
+  name: "Resume"
+}
+</script>
+
+<style scoped>
+
+</style>
